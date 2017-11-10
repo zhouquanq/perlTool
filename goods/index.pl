e1de6420c4395bb2cb5d3046a8e905c7768331ef
zhouquan <306757130@qq.com>
2017-11-10 13:59:39 +0800

    优化

--- goods/index.pl
+++ goods/index.pl
@@ -11,23 +11,29 @@ my $userid = "root";            # 数据库用户名
 my $password = "";        # 数据库密码
 # 连接数据库
 my $dbh = DBI->connect($dsn, $userid, $password ) or die $DBI::errstr;
-# my $sth = $dbh->prepare("SELECT * FROM websites");   # 预处理 SQL  语句
+# 避免插入数据库乱码
+$dbh->do("SET NAMES utf8");
 
 open(FILE, "<iap.log");
 while (<FILE>) {
-	# if($_ =~ /([1-9]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) ([0-2]?\d):[0-5]\d:[0-5]\d,\d{3})(.+)Delay -> (\S+)/){
+    # 正则匹配所需要提取的值
 	if($_ =~ /([1-9]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) ([0-2]?\d):[0-5]\d:[0-5]\d)(.+)amount=(\w+)(.+)orderId=(\w+)(.+)regionId=(\w+)(.+)price=(\w+)(.+)purchaseId=(\w+)(.+)goodsName=(.+),(.+)userId=(\d+)/){
         # print "$16 \n";
         # print $1,$6,$8,$10,$12,$14,$16,$18,"\n";
         # print "INSERT INTO goodslog
         #     (amout,orderId,regionId,price,purchaseId,goodsName,userId,time)
         #     values
-        #     ($6,$8,$10,$12,$14,$16,$18,$1);";
+        #     ($6,$8,$10,$12,$14,$16,$18,$1);";\
 
+        # 使用 prepare() API 预处理 SQL 语句
         my $sth = $dbh->prepare("INSERT INTO goodslog (amout,orderId,regionId,price,purchaseId,goodsName,userId,time) values ($6,'$8',$10,$12,$14,'$16',$18,'$1');");
+        # 使用 execute() API 执行 SQL 语句
         $sth->execute() or die $DBI::errstr;
+        # 使用 finish() API 释放语句句柄
         $sth->finish();
-        $dbh->commit;
 	}
 }
+# 隐藏自动提交错误提示
+$dbh->{Warn} = 0;
+$dbh->commit;
 $dbh->disconnect();
